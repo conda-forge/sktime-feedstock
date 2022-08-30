d57f5daad485bb1f4679ca85b8111b0d04e0481f
TTran92 <tran.aggies@gmail.com>
2022-08-30 00:35:58 -0500

    try build.sh

---
+++ recipe/build.sh
@@ -0,0 +1,3 @@
+pip install https://files.pythonhosted.org/packages/9e/28/99e03296f0e936fc6feca57511e8a7d0d52e7c923953bffc52800fd28a0c/gluonts-0.9.0-py3-none-any.whl
+pip install https://files.pythonhosted.org/packages/fa/5b/620f273ddb2fb68cbe2a9751c243381f9b77c6695d67e20e19766b40389b/kotsu-0.3.1-py3-none-any.whl
+pip install https://files.pythonhosted.org/packages/5f/13/ce03c62e71ab14b6e14f6073e2f123aa7ea155d514583aba60289bc22aad/esig-0.9.7-cp39-cp39-manylinux2010_x86_64.whl
